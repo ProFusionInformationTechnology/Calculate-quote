c931d097de6dc6720e5330d3017eb22bb673af13
DESKTOP-D6J8718\USER <pfcc00001@hotmail.com>
2023-10-03 10:30:57 +0800

    [修正] 將專案預設使用的 sass 變更為 scss

--- src/app/component/block-area/block-area.component.ts
+++ src/app/component/block-area/block-area.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-block-area',
   templateUrl: './block-area.component.html',
-  styleUrls: ['./block-area.component.sass']
+  styleUrls: ['./block-area.component.scss']
 })
 export class BlockAreaComponent {
 
