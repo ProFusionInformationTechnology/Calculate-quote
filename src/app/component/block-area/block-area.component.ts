a807133700a48b5dc50327d3d0ca17b551c88650
DESKTOP-D6J8718\USER <pfcc00001@hotmail.com>
2023-10-06 09:38:10 +0800

    [新增] 在區塊中建置該區塊的 list 物件

--- src/app/component/block-area/block-area.component.ts
+++ src/app/component/block-area/block-area.component.ts
@@ -1,14 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-block-area',
   templateUrl: './block-area.component.html',
   styleUrls: ['./block-area.component.scss']
 })
-export class BlockAreaComponent {
+export class BlockAreaComponent implements OnInit  {
+  ngOnInit(): void {
+    this.list.id = this.id;
+    this.list.name = `項目 ${this.id}`;
+  }
   @Input() id:Number = 0;
+
+  public list = {
+    id: this.id,
+    value: [
+      {
+        name: '請輸入名稱',
+        num: 0,
+        orPrice: 10,
+        caPrice: 10,
+        orTotal: 0,
+        caTotal: 0,
+      }
+    ],
+    name: `項目 ${this.id}`,
+    orTotal: 0,
+    caTotal: 0,
+  }
 
   public consoleValue(e:Event) {
     console.error((e.target as HTMLInputElement).value)
+    console.error(this.list)
   }
 }
