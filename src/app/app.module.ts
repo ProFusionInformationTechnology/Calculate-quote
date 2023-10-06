fec751ab0c3386957d5d3b68073005208b588b01
DESKTOP-D6J8718\USER <pfcc00001@hotmail.com>
2023-10-06 10:35:10 +0800

    [新增] FormsModule 以及 ngFOr 迴圈

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BlockAreaComponent } from './component/block-area/block-area.component';
@@ -10,7 +11,8 @@ import { BlockAreaComponent } from './component/block-area/block-area.component'
     BlockAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
