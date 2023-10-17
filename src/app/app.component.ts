0a8a1a9a90a6b0a7ebf5b3100d51593430ed2da9
DESKTOP-D6J8718\USER <pfcc00001@hotmail.com>
2023-10-17 11:59:55 +0800

    [新增] 增加參數儲存各區塊回傳的數值

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public blockArrayNum:Array<Number> = [1, 2, 3];
+  public blockArrayNum:Array<Number> = [1];
+  public blockArrayAll = [{
+    name: '未命名',
+    id: 1,
+    orTotal: 0,
+    caTotal: 0
+  }];
 
   public addBlock() {
-    this.blockArrayNum.push(this.blockArrayNum.length + 1)
+    this.blockArrayNum.push(this.blockArrayNum.length + 1);
+    const obj = {
+      name: '未命名',
+      id: this.blockArrayNum.length,
+      orTotal: 0,
+      caTotal: 0
+    };
+    this.blockArrayAll.push(obj);
   }
 }
